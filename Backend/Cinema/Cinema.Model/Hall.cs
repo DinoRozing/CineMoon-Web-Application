30fc9fa7653fe329e371f135ff9831683071295d
DinoRozing <dino.rozing555@gmail.com>
2024-06-11 09:47:45 +0200

    Created models

---
+++ Backend/Cinema/Cinema.Model/Hall.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cinema.Model
+{
+    public class Hall
+    {
+        public Guid Id { get; set; }
+        public string? HallNumber { get; set; }
+
+        public ICollection<Seat>? Seats { get; set; }
+        public ICollection<Projection>? Projections { get; set; }
+    }
+}
---
+++ Backend/Cinema/Cinema.Model/Review.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cinema.Model
+{
+    public class Review
+    {
+        public Guid Id { get; set; }
+        public string? Description { get; set; }
+        public int Rating { get; set; }
+        public Guid UserId { get; set; }
+        public Guid MovieId { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateUpdated { get; set; }
+        public Guid CreatedByUserId { get; set; }
+        public Guid UpdatedByUserId { get; set; }
+
+        public User? User { get; set; }
+        public Movie? Movie { get; set; }
+    }
+}
