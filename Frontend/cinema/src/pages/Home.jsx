f6fcf286f1834b9e8eb08d8829c743446e797abc
Karlo Buhinjak <buhinjak.k@gmail.com>
2024-06-27 09:53:38 +0200

    added bootstrap

--- Frontend/cinema/src/pages/Home.jsx
+++ Frontend/cinema/src/pages/Home.jsx
@@ -1,7 +1,13 @@
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div>
+      <div class="alert alert-primary" role="alert">
+        A simple primary alert—check it out!
+      </div>
+    </div>
+  );
 };
 
 export default Home;
